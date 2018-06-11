9cccb124750fb6ef648bc2a76a071d05d5b263fd
Fernando Tejada <rtejada@gmu.edu>
2018-06-11 12:10:53 -0400

    starter project

---
+++ src/main/java/com/rtsolutions/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.rtsolutions.springdemo.dao;
+
+import java.util.List;
+
+import com.rtsolutions.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
